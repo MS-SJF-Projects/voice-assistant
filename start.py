1510ff7484e0f54d4be208e2eebd379bbb84b4a9
Ioana Amarande <ioanaa@microsoft.com>
2022-09-12 10:12:50 +0200

    Prepare the repo

--- start.py
+++
@@ -1,6 +0,0 @@
-# Replace the None values in the following variables according to README.md
-speech_key = None
-language_key = None
-language_region = 'westeurope'
-language_endpoint = None
-language_project = 'sjf22-clu'
